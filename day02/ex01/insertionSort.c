08bb65072efdf2368934321c7c5e5c859a9b42a6
Alexander <alexandercarlson99@gmail.com>
2019-06-12 20:18:07 -0700

    Added bigo

--- day02/ex01/insertionSort.c
+++ day02/ex01/insertionSort.c
@@ -8,7 +8,7 @@ void insertionSort(struct s_player **players) {
 	for (unsigned i = 1; players[i]; ++i) {
 		if (i < 1)
 			i = 1;
-		if (players[i]->score < players[i-1]->score) {
+		if (players[i]->score > players[i-1]->score) {
 			tmp = players[i];
 			players[i] = players[i-1];
 			players[i-1] = tmp;
