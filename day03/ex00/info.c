52476ca3e5eb1ee515827b4f60503d02be1a2a31
Alexander <alexandercarlson99@gmail.com>
2019-06-13 22:57:29 -0700

    Yee

--- day03/ex00/info.c
+++ day03/ex00/info.c
@@ -1,4 +1,11 @@
 #include "header.h"
 
+int traverseTree(struct s_node *root, struct s_info *info) {
+
+}
+
 struct s_info getInfo(struct s_node *root) {
+	struct s_info info;
+
+	traverseTree(root, &info);
 }
