2db76c29a778778c6d55c94068cd62a14b465024
Alexander <alexandercarlson99@gmail.com>
2019-06-18 10:35:07 -0700

    Easy

--- day05/ex00/possibleSlices.c
+++ day05/ex00/possibleSlices.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "header.h"
 
-void rec(int target, int n, struct s_array *array, int total) {
+void rec(int target, struct s_array *array, int total) {
 	if (total == target) {
 		arrayPrint(array);
 		return ;
@@ -9,15 +9,15 @@ void rec(int target, int n, struct s_array *array, int total) {
 	else if (total > target)
 		return ;
 	for (int i = 0; i < target; ++i) {
-		if (n - i <= 0)
+		if (target - i <= 0)
 			break ;
-		arrayAppend(array, n - i);
-		rec(target, n - i, array, total + (n - i));
+		arrayAppend(array, target - i);
+		rec(target, array, total + (target - i));
 		--array->length;
 	}
 }
 
 void printPossibleSlices(int pizzaSize) {
 	struct s_array *array = arrayInit();
-	rec(pizzaSize, pizzaSize, array, 0);
+	rec(pizzaSize, array, 0);
 }
