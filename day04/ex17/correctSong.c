99e2ccbf0fda48d216ca56b05c5ff9e3108b515d
Alexander <alexandercarlson99@gmail.com>
2019-06-18 15:27:02 -0700

    Fixed boo boo

--- day04/ex17/correctSong.c
+++ day04/ex17/correctSong.c
@@ -4,5 +4,5 @@ void correctSong(struct s_bit *bit, int l, int row, int col, int dist) {
 	if (!bit || !bit->arr || row >= l)
 		return ;
 	for (int i = 0; i < dist && row + i < bit->n; ++i)
-		bit->arr[row + i] = 1 << col;
+		bit->arr[row + i] |= 1 << col;
 }
