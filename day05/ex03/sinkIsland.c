416c0f1958c269f0da4a34c08ed4600461137fa3
Alexander <alexandercarlson99@gmail.com>
2019-06-18 12:04:36 -0700

    Easy

---
+++ day05/ex03/sinkIsland.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sinkIsland.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/18 11:58:54 by acarlson          #+#    #+#             */
+/*   Updated: 2019/06/18 12:04:15 by acarlson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header.h"
+
+void sinkIsland(int **map, int row, int col) {
+	if (!map || !map[row] || map[row][col] != 1)
+		return ;
+	map[row][col] = 0;
+	sinkIsland(map, row + 1, col);
+	sinkIsland(map, row - 1, col);
+	sinkIsland(map, row, col + 1);
+	sinkIsland(map, row, col - 1);
+}
