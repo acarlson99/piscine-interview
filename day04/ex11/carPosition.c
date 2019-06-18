2b3eb15457c4d9cbe0eaed7de17635a72f644b76
Alexander <alexandercarlson99@gmail.com>
2019-06-17 17:32:44 -0700

    Idk dude...

--- day04/ex11/carPosition.c
+++ day04/ex11/carPosition.c
@@ -1,5 +1,31 @@
 #include "header.h"
 
-int carPosition(unsigned parkingRow) {
+int inc(int n) {
+	if (n & 1)
+		return (inc(n >> 1) << 1);
+	return (n | 1);
+}
 
+int rec(unsigned parkingRow) {
+	if (parkingRow >> 1)
+		return (inc(rec(parkingRow >> 1)));
+	return (0);
+}
+
+int powOf2(unsigned bits) {
+	if (bits) {
+		if (bits >> 1) {
+			if (bits & 1)
+				return (0);
+			return (powOf2(bits >> 1));
+		}
+		return (1);
+	}
+	return (0);
+}
+
+int carPosition(unsigned parkingRow) {
+	if (powOf2(parkingRow))
+		return (rec(parkingRow));
+	return (-1);
 }
