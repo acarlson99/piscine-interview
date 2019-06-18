79143b923c6be968fe448a446d0839741faa10ca
Alexander <alexandercarlson99@gmail.com>
2019-06-17 19:19:49 -0700

    Added bigo files and finished longestSequence

--- day04/ex15/longestSequence.c
+++ day04/ex15/longestSequence.c
@@ -1,4 +1,17 @@
 #include "header.h"
 
+int inc(int n) {
+	if (n & 1)
+		return (inc(n >> 1) << 1);
+	return (n | 1);
+}
+
+int rec(unsigned bits, int acc) {
+	if (bits)
+		return (rec(bits & (bits << 1), inc(acc)));
+	return (acc);
+}
+
 int longestSequence(unsigned parkingRow) {
+	return (rec(parkingRow, 0));
 }
