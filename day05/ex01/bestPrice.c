d014ef1d6640dacfa8f4d0b2df5d436072c76e91
Alexander <alexandercarlson99@gmail.com>
2019-06-18 11:40:20 -0700

    Nailed him - er... I mean nailed *it*

---
+++ day05/ex01/bestPrice.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bestPrice.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/18 11:08:36 by acarlson          #+#    #+#             */
+/*   Updated: 2019/06/18 11:37:57 by acarlson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header.h"
+
+double max_(double a, double b) {
+	return (a > b ? a : b);
+}
+
+double rec(int n, double currentPrice, double *prices) {
+	double max = currentPrice;
+	int i = 0;
+	
+	while (++i <= n)
+		max = max_(rec(n - i, currentPrice + prices[i], prices), max);
+	return (max);
+}
+
+double bestPrice(int pizzaSize, double *prices) {
+	return (rec(pizzaSize, 0., prices));
+}
