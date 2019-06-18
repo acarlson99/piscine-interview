26072403a2cfbabe7da2ffe475956273e0b7f95f
Alexander <alexandercarlson99@gmail.com>
2019-06-18 11:56:17 -0700

    Yoink'd it

--- day05/ex02/main.c
+++ day05/ex02/main.c
@@ -19,6 +19,9 @@ int main(int ac, char **av)
 	--------------------*/
         printf("%d : %.2f\n", pizzaSize, optimizedBestPrice(pizzaSize, pricesCont->items));
 
+        pizzaSize = 99;
+        printf("%d : %.2f\n", pizzaSize, optimizedBestPrice(pizzaSize, pricesCont->items));
+
 	return (0);
 }
 
