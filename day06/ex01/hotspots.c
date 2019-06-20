184aa51853b8bd2b21a04f9d86f94d7fa50d9fc2
John <john@Jellybean.localdomain>
2019-06-20 16:54:41 -0700

    ?

--- day06/ex01/hotspots.c
+++ day06/ex01/hotspots.c
@@ -1,5 +1,18 @@
-#include <stdlib.h>
 #include "header.h"
 
 int selectHotspots(struct s_hotspot **hotspots) {
+	int ret;
+	int maxrange;
+
+	if (!hotspots)
+		return (0);
+	ret = 1;
+	maxrange = 0;
+	for (int i = 1; hotspots[i]; i++) {
+		if (hotspots[i]->pos - hotspots[i]->radius >= maxrange) {
+			maxrange = hotspots[i]->pos + hotspots[i]->radius;
+			ret++;
+		}
+	}
+	return (ret);
 }
