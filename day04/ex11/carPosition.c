7a581110f1213e6d8465636641afcc6f82980733
Alexander <alexandercarlson99@gmail.com>
2019-06-20 19:03:26 -0700

    Removed silly

--- day04/ex11/carPosition.c
+++ day04/ex11/carPosition.c
@@ -1,18 +1,6 @@
 #include <math.h>
 #include "header.h"
 
-int powOf2(unsigned bits) {
-	if (bits) {
-		if (bits >> 1) {
-			if (bits & 1)
-				return (0);
-			return (powOf2(bits >> 1));
-		}
-		return (1);
-	}
-	return (0);
-}
-
 int carPosition(unsigned parkingRow) {
 	double r = log2(parkingRow);
 	if (modf(r, &r))
